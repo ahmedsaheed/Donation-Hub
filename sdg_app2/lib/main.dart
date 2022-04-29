4dc96c2034d46a783168fe19198ee46bd8713457
ahmedsaheed <ahmedsaheed2@outlook.com>
2022-04-29 15:43:25 +0100

    Added Config

--- sdg_app2/lib/main.dart
+++ sdg_app2/lib/main.dart
@@ -12,6 +12,7 @@ import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 bool get isIos => foundation.defaultTargetPlatform == TargetPlatform.iOS;
 
